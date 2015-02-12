633da48d21c03d78ab9eca16fce24b7545165b89
w32backdoor <harisbarki@ymail.com>
2015-02-12 18:02:30 -0330

    added some options

--- modules/fetch.py
+++ modules/fetch.py
@@ -1,5 +1,6 @@
 import willie.module
 import re
+from random import randint
 
 count = 0		#get tired after some time
 
@@ -7,14 +8,24 @@ count = 0		#get tired after some time
 @willie.module.commands('fetch')
 @willie.module.rule('throw?')
 @willie.module.rule('fetch?')
-def throwcommand(bot, trigger):
+def throwcommand(bot, trigger):	
 	global count
-	if count == 3:
-		bot.action('Tired')
-		count = 0	
-	else:
-		words = re.split(' ' , trigger)
-		del words[0]
-		joined_words = ' '.join(words)
-		bot.action(' chases after ' + joined_words)
-		count = count + 1
+
+	x = randint(0,5)	#inclusive
+	words = re.split(' ' , trigger)
+	del words[0]
+	joined_words = ' '.join(words)
+
+	if( x < 3):
+		if count == 3:
+			bot.action('Tired')
+			count = 0	
+		else:
+			bot.action('chases after ' + joined_words)
+			count = count + 1
+	elif (x == 4):
+		bot.action('didn\'t see the ' + joined_words + ', chases squirrel instead')
+	elif (x == 5):
+		bot.action('is hungry!!! (dont want to fetch)')
+	
+		
