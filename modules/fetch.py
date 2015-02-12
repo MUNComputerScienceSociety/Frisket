d9349a3c36e07d7004d3ff1f4665ba376b86b294
w32backdoor <harisbarki@ymail.com>
2015-02-12 19:37:15 -0330

    adding some more functions

--- modules/fetch.py
+++ modules/fetch.py
@@ -2,8 +2,9 @@ import willie.module
 import re
 from random import randint
 
-count = 0		#get tired after some time
+count = randint(2,5)		#get tired after some time (randomized)
 hungry = False
+
 
 @willie.module.commands('throw')
 @willie.module.commands('fetch')
@@ -14,30 +15,54 @@ def throwcommand(bot, trigger):
 	global hungry
 
 	if(hungry == True):
-		bot.action('can\'t do anything until fed!')
-	else:
-		x = randint(0,5)	#inclusive
-		words = re.split(' ' , trigger)
-		del words[0]
-		joined_words = ' '.join(words)
-		
-		if( x < 4):
-			if count == 3:
-				bot.action('Tired')
-				count = 0
-			else:
-				bot.action('chases after ' + joined_words)
-				count = count + 1
-		elif (x == 4):
-			bot.action('didn\'t see the ' + joined_words + ', chases squirrel instead')
-		elif (x == 5):
-			bot.action('is hungry!!! (dont want to fetch)')
-			hungry = True
+		HungryMessage(bot)
+		return
+	
+	x = randint(0,5)	#inclusive
+	words = re.split(' ' , trigger)
+	del words[0]
+	joined_words = ' '.join(words)
+
+	if count == 0:
+		bot.action('Tired')
+		Tired(bot, trigger)
+	elif(x < 3):
+		bot.action('chases after ' + joined_words)
+		count -= 1
+	elif (x == 3):
+		Rest(bot, trigger)
+	elif (x == 4):
+		bot.action('didn\'t see the ' + joined_words + ', chases squirrel instead')
+		count -= 1
+	elif (x == 5):
+		bot.action('is hungry!!! (dont want to fetch)')
+		hungry = True
 
 
 @willie.module.rule('feed')
-def welcome(bot, trigger):
+def Feed(bot, trigger):
 	global hungry
 	bot.action('woof woof!')
 	hungry = False
 	
+def HungryMessage(bot):
+	bot.action('can\'t do anything until fed!')
+
+def Tired(bot, trigger):
+	global count
+	bot.action('Sleepy')
+	Rest(bot, trigger)
+	count = randint(2,5)
+
+@willie.module.rule('rest')
+@willie.module.rule('sleep')
+def Rest(bot, trigger):
+	global count
+	bot.action('zzzzzzzzzz!!!!')
+	count = randint(2,5)
+
+@willie.module.rule('pets')
+def Pets(bot, trigger):
+	bot.action('woooof wooooof')
+
+
