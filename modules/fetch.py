0b9e7fc1fd6719cee763721f09c59a82141db776
Rebecca Hynes <rdh080@mun.ca>
2015-02-19 17:32:17 -0330

    Removed hunger

--- modules/fetch.py
+++ modules/fetch.py
@@ -3,66 +3,44 @@ import re
 from random import randint
 
 count = randint(2,5)		#get tired after some time (randomized)
-hungry = False
-
 
 @willie.module.commands('throw')
 @willie.module.commands('fetch')
 @willie.module.rule('throw?')
 @willie.module.rule('fetch?')
 def throwcommand(bot, trigger):	
-	global count
-	global hungry
+    global count
 
-	if(hungry == True):
-		HungryMessage(bot)
-		return
-	
-	x = randint(0,5)	#inclusive
-	words = re.split(' ' , trigger)
-	del words[0]
-	joined_words = ' '.join(words)
+    x = randint(0,4)        #inclusive
+    words = re.split(' ' , trigger)
+    del words[0]
+    joined_words = ' '.join(words)
 
-	if count == 0:
-		bot.action('Tired')
-		Tired(bot, trigger)
-	elif(x < 3):
-		bot.action('chases after ' + joined_words)
-		count -= 1
-	elif (x == 3):
-		Rest(bot, trigger)
-	elif (x == 4):
-		bot.action('didn\'t see the ' + joined_words + ', chases squirrel instead')
-		count -= 1
-	elif (x == 5):
-		bot.action('is hungry!!! (dont want to fetch)')
-		hungry = True
-
-
-@willie.module.rule('feed')
-def Feed(bot, trigger):
-	global hungry
-	bot.action('woof woof!')
-	hungry = False
-	
-def HungryMessage(bot):
-	bot.action('can\'t do anything until fed!')
+    if count == 0:
+        bot.action('Tired')
+        Tired(bot, trigger)
+    elif(x < 3):
+        bot.action('chases after ' + joined_words)
+        count -= 1
+    elif (x == 3):
+        Rest(bot, trigger)
+    elif (x == 4):
+        bot.action('didn\'t see the ' + joined_words + ', chases squirrel instead')
+        count -= 1
 
 def Tired(bot, trigger):
-	global count
-	bot.action('Sleepy')
-	Rest(bot, trigger)
-	count = randint(2,5)
+    global count
+    bot.action('Sleepy')
+    Rest(bot, trigger)
+    count = randint(2,5)
 
 @willie.module.rule('rest')
 @willie.module.rule('sleep')
 def Rest(bot, trigger):
-	global count
-	bot.action('zzzzzzzzzz!!!!')
-	count = randint(2,5)
+    global count
+    bot.action('zzzzzzzzzz!!!!')
+    count = randint(2,5)
 
 @willie.module.rule('pets')
 def Pets(bot, trigger):
-	bot.action('woooof wooooof')
-
-
+    bot.action('woooof wooooof')
