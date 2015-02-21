f75aa1e8ac5fc20c25302325e0307987f128caa7
Mark Stacey <markjstacey@gmail.com>
2015-02-20 20:52:06 -0330

    Bury games memory inside 'games' dict.

--- modules/games.py
+++ modules/games.py
@@ -14,7 +14,7 @@ from willie.formatting import bold
 
 #TODO: set in 'configure' function
 command_prefix = '!'
-game_channels = ['#muncs-games', '#mainframe']
+game_channels = ['#muncs-games', '#mainframe', '#gamecube']
 
 
 @willie.module.rule('^' + command_prefix + 'game\s(\w*)')
@@ -25,13 +25,16 @@ def start_game(bot, trigger):
     elif not trigger.sender in game_channels:
         bot.reply("Games are not allowed in this channel")
         return
-    elif trigger.sender in bot.memory and 'current_game' in bot.memory[trigger.sender]:
-        bot.reply("Cannot start game; already one running: " + bot.memory[trigger.sender]['current_game'])
+    elif 'game' in bot.memory and trigger.sender in bot.memory['game'] and 'current_game' in bot.memory['game'][trigger.sender]:
+        bot.reply("Cannot start game; already one running: " + bot.memory['game'][trigger.sender]['current_game'])
         return
     
     bot.say(bold('WARNING:') + " Incoming game")
     bot.say("Starting " + trigger.group(1))
     
-    bot.memory[trigger.sender] = {'current_game': trigger.group(1)}
+    if not 'game' in bot.memory:
+        bot.memory['game'] = {}
+    
+    bot.memory['game'][trigger.sender] = {'current_game': trigger.group(1)}
     
     return
