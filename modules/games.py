58c31aeff70fe170ae98903f7f0ef55822bd87c1
Mark Stacey <markjstacey@gmail.com>
2015-02-20 21:29:38 -0330

    Add list of valid games. Simplify logic for start/end game

--- modules/games.py
+++ modules/games.py
@@ -15,7 +15,7 @@ from willie.formatting import bold
 #TODO: set in 'configure' function
 command_prefix = '!'
 game_channels = ['#muncs-games', '#mainframe', '#gamecube']
-
+valid_games = ['rockpapersissors']
 
 @willie.module.rule('^' + command_prefix + 'start\s(\w+)')
 def start_game(bot, trigger):
@@ -25,7 +25,7 @@ def start_game(bot, trigger):
     elif not trigger.sender in game_channels:
         bot.reply("Games are not allowed in this channel")
         return
-    elif 'game' in bot.memory and trigger.sender in bot.memory['game'] and 'current_game' in bot.memory['game'][trigger.sender]:
+    elif 'game' in bot.memory and trigger.sender in bot.memory['game']:
         bot.reply("Cannot start game; already one running: " + bot.memory['game'][trigger.sender]['current_game'])
         return
     
@@ -39,17 +39,18 @@ def start_game(bot, trigger):
     
     return
 
+
 @willie.module.rule('^' + command_prefix + 'end')
 def end_game(bot, trigger):
     if trigger.is_privmsg:
         bot.reply("This command only works in channels")
         return
-    elif not 'game' in bot.memory or not trigger.sender in bot.memory['game'] or not 'current_game' in bot.memory['game'][trigger.sender]:
+    elif not 'game' in bot.memory or not trigger.sender in bot.memory['game']:
         bot.reply("There is no current game to end")
         return
     
     bot.say("Game over")
     
-    del bot.memory['game'][trigger.sender]['current_game']
+    del bot.memory['game'][trigger.sender]
     
     return
