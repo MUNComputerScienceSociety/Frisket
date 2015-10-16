d91986fd8926e260a02cdce220c09034e21482a6
Karoliina Oksanen <hkoksanen@gmail.com>
2015-10-16 17:48:10 -0230

    Switch to forecast.io API; check response status code; also display probability of precipitation

--- modules/munweather.py
+++ modules/munweather.py
@@ -1,42 +1,47 @@
 # -*- coding: utf-8 -*-
 import willie.module
-import willie.web
+import urllib2
 import json
 from willie.formatting import bold
 
+API_KEY = 'put forecast.io API key here'
+
 @willie.module.commands('munweather')
-def munweather(bot, trigger):	
+def munweather(bot, trigger):
     """
     munweather - gets the bot to tell you the current weather at the nearest weather station to the
-    MUN Engineering building coordinates. weather data is retrieved from the Open Weather API.
+    MUN Engineering building coordinates. Weather data is retrieved from The Dark Sky Forecast API.
     """
 
-    url = "http://api.openweathermap.org/data/2.5/weather?lat=47.574037&lon=-52.734607"
+    url = 'https://api.forecast.io/forecast/{}/47.574037,-52.734607?units=ca'.format(API_KEY)
 
-    # parse json into a python dict
-    data = json.loads(willie.web.get(url))
-    # get string description and current temperature in Celsius
-    description = data['weather'][0]['description']
-    temp = data['main']['temp'] - 273.15
-    # get humidity and wind speed in km/h
-    humidity = data['main']['humidity']
-    windspeed = data['wind']['speed'] * 3.6
+    response = urllib2.urlopen(url)
+    if response.getcode() != 200:
+        bot.say('There is no weather at MUN! :O')
+        return
+    data = json.load(response)
 
-    answer = "The current weather is {}! It is {:.1f}\N{DEGREE SIGN}C ".format(description,
-            temp)
+    description = data['currently']['summary']
+    temp = data['currently']['temperature']
+    humidity = data['currently']['humidity'] * 100
+    windspeed = data['currently']['windSpeed']
+    precip_probability = data['currently']['precipProbability'] * 100
+
+    answer = 'The current weather is {}! It is {:.1f}\N{DEGREE SIGN}C '.format(description, temp)
     if(temp < -5):
-        answer += "cold! Brrr!"
+        answer += 'cold! Brrr!'
     elif(temp >= -5 and temp < 5):
-        answer += "chilly!"
+        answer += 'chilly!'
     elif(temp >= 5 and temp < 20):
-        answer += "! Rather pleasant!"
+        answer += '! Rather pleasant!'
     elif(temp >= 20 and temp < 25):
-        answer += " warm!"
+        answer += 'warm!'
     else:
-        answer += " hot!"
+        answer += 'hot!'
 
-    answer2 = "The humidity is {}% and the wind speed is {:.1f} km/h.".format(humidity, windspeed) 
+    answer2 = 'The humidity is {}% and the wind speed is {:.1f} km/h.'.format(humidity, windspeed)
+    answer2 += ' The probability of precipitation is {}%!'.format(precip_probability)
 
     bot.say(answer)
     bot.say(answer2)
-    bot.say(bold("Woof!")) 
+    bot.say(bold('Woof!'))
